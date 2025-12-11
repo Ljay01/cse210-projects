be396d41d4287437c88f7d8dbe5aaaf1f746264b
lawrencejuma <lawrenceju6@gmail.com>
2025-12-12 02:54:31 +0300

    Week 7 project.

---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,22 @@
+public class Running : Activity
+{
+    private double _distanceMiles;
+
+    public Running(DateTime date, int minutes, double distanceMiles)
+        : base(date, minutes)
+    {
+        _distanceMiles = distanceMiles;
+    }
+
+    public override double GetDistance() => _distanceMiles;
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+}
